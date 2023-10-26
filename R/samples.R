b0c6ccf2ff1aa5164e815217294ae99b07baaf7e
Rebecca Fisher <R.Fisher@aims.gov.au>
2023-10-26 16:38:26 +0800

    Add function for returing samples of hc estimates

---
+++ R/samples.R
@@ -0,0 +1,35 @@
+
+
+
+xsample_estimates <- function(x, args, what) {
+  if (grepl("^ssd_p", what)) {
+    args$q <- x
+  } else {
+    args$p <- x
+  }
+  samples <- do.call(what, args)
+  samples
+}
+
+sample_estimates <- function(estimates, what, x, .names = NULL) {
+  args <- transpose(estimates, .names = .names)
+  args <- purrr::map_depth(args, 2, function(x) {
+    if (is.null(x)) NA_real_ else x
+  })
+  args <- lapply(args, as.double)
+  x <- lapply(x, xsample_estimates, args, what)
+  x
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
